88f16bb0c7a82e55be0a5905368c0a50fa210b90
auspicious <108572025+auspicious0@users.noreply.github.com>
2023-06-30 19:19:02 +0900

    wrong answer 009
    so difficult question. what is missed thing to the question...

---
+++ wa009.py
@@ -0,0 +1,24 @@
+import itertools
+
+m,n=map(int,input().split())
+s=list(map(str,input()))
+dnastr=[('A',0),('C',0),('G',0),('T',0)]
+dna=list(map(int,input().split()))
+s.sort()
+
+for i in s:
+    for j in dnastr:
+        if i==j[0]:
+            j[1]+=1
+result=1
+for i in range(4):
+    if dnastr[i][1]<dna[i]:
+        result=0
+        break
+    k=len(list(itertools.combinations(dnastr[i][1],dna[i])))
+    result*=k
+    n-=k
+if n>0:
+    #도저히 모르겠다!
+
+print(s)
