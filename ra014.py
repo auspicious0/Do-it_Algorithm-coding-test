ec6f9e98190c1e991bdad0243cf4faacf19b4f28
auspicious <108572025+auspicious0@users.noreply.github.com>
2023-07-03 15:30:20 +0900

    right answer 014
    백준 사이트 pypy3로 하니 계속 시간초과가 났다. 코드에 논리적 오류를 찾느라 2시간을 소모했지만 아무리 봐도 없었다. 무심코 pypy3을 python으로 바꾸니 정상동작하였다... 슬프다..

---
+++ ra014.py
@@ -0,0 +1,18 @@
+from queue import PriorityQueue
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+numbers=PriorityQueue()
+
+for i in range(n):
+    k=int(input())
+    if k==0:
+        if numbers.empty():
+            print(0)
+        else:
+            print(numbers.get()[1])
+    else:
+        numbers.put((abs(k),k))
+
+        
