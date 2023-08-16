8887dc949fbde74a3b20550b3d0a061f3198eb63
auspicious <108572025+auspicious0@users.noreply.github.com>
2023-08-17 03:21:12 +0900

    ccw 알고리즘
    앞의 작은 실수가 있었다. ccw결과가 0보다 크면 반시계 방향으로 점이 형성된다는 점을 간과했다.

--- ra097.py
+++ ra097.py
@@ -5,8 +5,8 @@ x2,y2=map(int,input().split())
 x3,y3=map(int,input().split())
 result=(x1*y2+x2*y3+x3*y1)-(x1*y3+x3*y2+x2*y1)
 if result>0:
-    print(-1)
-elif result<0:
     print(1)
+elif result<0:
+    print(-1)
 else:
     print(0)
